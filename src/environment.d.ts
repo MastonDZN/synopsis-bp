ac60399e424d6a18248bbba97370e6529cbc926b
Maston <MastonDZN@users.noreply.github.com>
2022-04-17 04:06:07 +0300

    change example files/functions

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
-    interface ProcessEnvironment {
+    // eslint-disable-next-line unicorn/prevent-abbreviations
+    interface ProcessEnv {
         readonly NODE_ENV?: 'development' | 'production';
         readonly EXAMPLE_VAR?: 'EXAMPLE_VALUE';
     }
